1e727c0f417973cd3a4173ecbfd74abaa307bf40
huangjinfu <jinfu.huang@shanbay.com>
2018-11-20 09:04:59 +0800

    [WS-][Words] add widget

--- app/src/main/java/cn/hjf/tmcrm/widget/ExpandAllGridView.java
+++ app/src/main/java/cn/hjf/tmcrm/widget/ExpandAllGridView.java
@@ -2,19 +2,19 @@ package cn.hjf.tmcrm.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.ListView;
+import android.widget.GridView;
 
-public class ExpandAllListView extends ListView {
+public class ExpandAllGridView extends GridView {
 
-	public ExpandAllListView(Context context) {
+	public ExpandAllGridView(Context context) {
 		super(context);
 	}
 
-	public ExpandAllListView(Context context, AttributeSet attrs) {
+	public ExpandAllGridView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
-	public ExpandAllListView(Context context, AttributeSet attrs, int defStyle) {
+	public ExpandAllGridView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
 
